854036ad27f158dbf80500ff63ac78f7b41f3687
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-07-23 14:41:08 +0200

    fix: plot accuracy and validation options

--- helper.py
+++ helper.py
@@ -6,14 +6,17 @@ def plot_training(history):
     plt.figure(figsize=(14, 4))
     plt.subplot(121)
     plt.plot(history.history['loss'], label='Training')
-    plt.plot(history.history['val_loss'], label='Validation')
+    if 'val_loss' in history.history:
+        plt.plot(history.history['val_loss'], label='Validation')
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.legend()
 
-    plt.subplot(122)
-    plt.plot(history.history['acc'], label='Training')
-    plt.plot(history.history['val_acc'], label='Validation')
-    plt.xlabel('epoch')
-    plt.ylabel('accuracy')
-    plt.legend()
+    if 'acc' in history.history:
+        plt.subplot(122)
+        plt.plot(history.history['acc'], label='Training')
+        if 'val_acc' in history.history:
+            plt.plot(history.history['val_acc'], label='Validation')
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.legend()
