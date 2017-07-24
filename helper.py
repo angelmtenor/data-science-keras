bab213aaf29694c4bd59d41b53bb4bde0e755ec0
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-07-24 13:25:48 +0200

    feat: plot training results in helper

--- helper.py
+++ helper.py
@@ -1,22 +1,46 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-def plot_training(history):
+def show_training(history):
+    """
+    Print the final loss and plot its evolution in the training process.
+    The same applies to 'validation loss', 'accuracy', and 'validation accuracy' if available
+    :param history: Keras history object (model.fit return)
+    :return:
+    """
+
+    hist = history.history
+
+    if 'loss' not in hist:
+        print("Error: 'loss' values not found in the history")
+        return
+
+    # show final results
+
+    print("Training loss: {:.4f}".format(hist['loss'][-1]))
+    if 'acc' in hist:
+        print("Training accuracy: {:.4f}".format(hist['acc'][-1]))
+
+    print("Validation loss: {:.2f}".format(hist['val_loss'][-1]))
+    if 'val_acc' in hist:
+        print("Validation accuracy: {:.2f}".format(hist['val_acc'][-1]))
+
+    # plot training
+
     plt.figure(figsize=(14, 4))
     plt.subplot(121)
-    plt.plot(history.history['loss'], label='Training')
-    if 'val_loss' in history.history:
-        plt.plot(history.history['val_loss'], label='Validation')
+    plt.plot(hist['loss'], label='Training')
+    if 'val_loss' in hist:
+        plt.plot(hist['val_loss'], label='Validation')
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.legend()
 
-    if 'acc' in history.history:
+    if 'acc' in hist:
         plt.subplot(122)
-        plt.plot(history.history['acc'], label='Training')
-        if 'val_acc' in history.history:
-            plt.plot(history.history['val_acc'], label='Validation')
+        plt.plot(hist['acc'], label='Training')
+        if 'val_acc' in hist:
+            plt.plot(hist['val_acc'], label='Validation')
         plt.xlabel('epoch')
         plt.ylabel('accuracy')
         plt.legend()
