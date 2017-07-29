d9c4836a40179914aa38d9fc549731428f37944f
angelmtenor <angelmtenor@gmail.com>
2017-07-29 09:33:19 +0200

    feat: Expand date into year, month, hour, weekday and holiday

--- helper.py
+++ helper.py
@@ -1,7 +1,10 @@
 """
 Helper module for Data-Science-Keras repository
 """
+
 import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 
 
 def show_training(history):
@@ -21,7 +24,7 @@ def show_training(history):
     # show final results
 
     print("\nTraining loss:  \t{:.4f}".format(hist['loss'][-1]))
-    if 'val_loss' in hist:    
+    if 'val_loss' in hist:
         print("Validation loss: \t{:.4f}".format(hist['val_loss'][-1]))
     if 'acc' in hist:
         print("\nTraining accuracy: \t{:.2f}".format(hist['acc'][-1]))
@@ -47,3 +50,51 @@ def show_training(history):
         plt.xlabel('epoch')
         plt.ylabel('accuracy')
         plt.legend()
+
+
+def expand_date(timeseries):
+    """
+    Expand pandas datetime series with 'Date' returning a dataframe with these columns:
+	- year : year
+	- month : month (1-12)
+	- hour : hour (0-23)
+  	- weekday : day of the week (0 Monday - 6 Sunday)
+	- holiday : weather day is holiday or not
+    """
+
+    assert type(
+        timeseries) == pd.core.series.Series, 'input must be pandas series'
+    assert timeseries.dtypes == 'datetime64[ns]', 'input must be pandas datetime'
+
+    date = timeseries.dt.date
+
+    print(date)
+
+    df = pd.DataFrame()
+    df['Year'] = pd.DatetimeIndex(date).year
+    df['Month'] = pd.DatetimeIndex(date).month
+    df['Day'] = pd.DatetimeIndex(date).day
+    df['Weekday'] = pd.DatetimeIndex(date).weekday
+
+    holidays = calendar().holidays(start=date.min(), end=date.max())
+    df['holiday'] = date.astype('datetime64').isin(holidays)
+
+    # df.drop('Date', axis='columns', inplace=True)
+    print("Holiday", holidays)
+
+    return df
+
+
+''' 
+    df = pd.DataFrame()
+    dates = ['2016-12-25 17:24:55', 
+            '2016-06-12 00:43:35',
+            '2016-01-19 11:35:24',
+            '2016-04-06 19:32:31',
+            '2016-02-15 13:30:55']
+
+    df['Dates'] =  pd.to_datetime(dates)
+
+    sample = expand_date(df['Dates'])
+    print(sample.head())
+'''
